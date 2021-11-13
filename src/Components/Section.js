a6e905ecd0f2ed570df8b018f5e152a4b2d2b997
Tuvshinbayar <tuvshinbayar.dev@gmail.com>
2021-11-14 00:02:55 +0800

    added

--- src/Components/Section.js
+++ src/Components/Section.js
@@ -12,8 +12,10 @@ const Section = () => {
                 <p className="font-medium mt-10 w-1/3 text-xl text-white hidden xl:block">
                     Таны төвлөрсөн бус, бие даасан санхүүгийн оролцооны гарц    
                 </p>
-                <div className=" w-60 h-11 bg-red-50 text-center flex justify-center items-center rounded-lg mt-10 hidden xl:block">
-                    <a href="#">Төсөлтэй танилцах</a>
+                <div className="hidden xl:block">
+                <div className=" w-60 h-11 bg-red-50 text-center flex justify-center items-center rounded-lg mt-10">
+                    <a href="#" className="text-center flex items-center justify-center">Төсөлтэй танилцах</a>
+                </div>
                 </div>
             </div>
         </div>
