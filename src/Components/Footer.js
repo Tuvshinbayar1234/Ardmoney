39bbb016159020af4b51d909dbf02ffdd315c208
mnkhod <mnkhod.dev@gmail.com>
2021-11-14 23:27:11 +0800

    Fixed Image Not Showing Problem And Footer Diverse

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,8 +4,8 @@ const Footer = () => {
     return(
         <div className="w-full h-full py-10" style={{backgroundColor: '#000000'}}>
             <div className="container mx-auto">
-                <h1 className="text-white font-semibold mx-2 text-sm xl:text-base">
-                    © Diverse Solutions All Rights Reserved.
+              <h1 className="mx-2 text-sm font-semibold text-center text-white md:text-left xl:text-base">
+                  <a href="https://www.dsolutions.mn/" target="_blank" className="text-blue-400 hover:text-blue-600">© Diverse Solutions.</a>
                 </h1>
             </div>
         </div>
--- src/Components/Soldonomics.js
+++ src/Components/Soldonomics.js
@@ -1,5 +1,3 @@
-import react from "react";
-import Chart from '../assets/tokenomic_chart.png'
 
 const Soldonomics = () => {
     return(
@@ -9,7 +7,7 @@ const Soldonomics = () => {
                     <h1 className="pb-16 text-2xl font-semibold text-center text-white xl:text-6xl">АрдМааний Тархалт</h1>
                 </div>
                 <div className="flex items-center justify-center mx-5 mt-10">
-                    <img src={Chart} />
+                    <img src="/chart.png" />
                 </div>
             </div>
         </div>
