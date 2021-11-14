fd0b079d9afe688f5dbd30c364f957a35e207d47
mnkhod <mnkhod.dev@gmail.com>
2021-11-14 23:51:48 +0800

    Fixed Responsive Navigation

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -2,27 +2,29 @@ import React from "react";
 import Hamburger from "./Hamburger";
 import Logo from '../../assets/logo.png'
 
-const Nav = () =>{
-    return(
-        <div className="fixed z-10 w-full py-8 bg-black bg-colors-ardm-black">
-            <div className="container flex justify-between w-full h-full px-5 mx-auto">
-                <div className="container relative flex items-center justify-between mx-auto">
-                    <img src={Logo} />
-                    <Hamburger />
-                </div>
-              <div className="flex flex-row justify-end hidden w-full text-sm font-light tracking-normal text-white lg:block">
-                    <a href="#Res" className="pl-4 hover:text-yellow-200">DAO гэж юу вэ?</a>
-                    <a href="#What" className="pl-4 hover:text-yellow-200">АрдМааний гэж юу вэ?</a>
-                    <a href="#Road" className="pl-4 hover:text-yellow-200">Төлөвлөгөө</a>
-                    <a href="#Eco" className="pl-4 hover:text-yellow-200">Экосистем</a>
-                    <a href="#Work" className="pl-4 hover:text-yellow-200">Баг</a>
-                    <a href="#FAQ" className="pl-4 hover:text-yellow-200">FAQ</a>
-                    <a href="/litepaper.pdf" className="pl-4 hover:text-yellow-200">Litepaper</a>
-                </div>
-            </div>
-            
+const Nav = () => {
+  let navTextStyle = `pl-1 lg:pl-4 hover:text-yellow-200`;
+
+  return (
+    <div className="fixed z-10 w-full py-8 bg-black bg-colors-ardm-black">
+      <div className="flex justify-center lg:justify-between">
+        <div className="flex items-center justify-center md:justify-start">
+          <img src={Logo} className="relative lg:left-40" />
+          <Hamburger />
         </div>
-    )
+        <div className="hidden text-sm font-light tracking-normal text-white md:mr-12 lg:block">
+          <a href="#Res" className={navTextStyle}>DAO гэж юу вэ?</a>
+          <a href="#What" className={navTextStyle}>АрдМааний гэж юу вэ?</a>
+          <a href="#Road" className={navTextStyle}>Төлөвлөгөө</a>
+          <a href="#Eco" className={navTextStyle}>Экосистем</a>
+          <a href="#Work" className={navTextStyle}>Баг</a>
+          <a href="#FAQ" className={navTextStyle}>FAQ</a>
+          <a href="/litepaper.pdf" className={navTextStyle}>Litepaper</a>
+        </div>
+      </div>
+
+    </div>
+  )
 }
 
 export default Nav
